f39682a8eae1e8cdbf840b0020e333a3812507b6
Edward-Brock <1416477069@qq.com>
2022-07-17 15:07:02 +0800

    部署测试空白页修复

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import routes from './routes' //导入router.js
 
 const router = createRouter({
-    history: createWebHistory(), //路由模式
+    history: createWebHistory('/'), //路由模式
     routes
 })
 
