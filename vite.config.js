b1e2a876deb5c719937924256ef6c948fd8d4888
Edward-Brock <1416477069@qq.com>
2022-07-18 22:27:25 +0800

    部署修复空白页面

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: import.meta.env.NODE_ENV === 'production'
-        ? './'
-        : '/',
+    base: './',
     server: {
         open: true,
         hmr: true
