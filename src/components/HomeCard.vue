c266a52e67fab199ed4accb0f90595ffd349815a
Edward-Brock <1416477069@qq.com>
2022-07-23 10:26:45 +0800

    从自建MySQL中读取数据内容 重写部分显示代码

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -4,31 +4,31 @@
         <span v-show="!card.isLoading">
         <!--标签区-->
         <div class="cardTagContainer">
-            <div class="cardTag" v-for="(cardGroup,index) in card.cardInfo" :key="cardGroup"
+            <div class="cardTag" v-for="(cardGroup,index) in card.partitionInfo" :key="cardGroup"
                  @click="goAnchor('#tag' + index)">
-                {{ cardGroup.title }}
+                {{ cardGroup.section_title }}
             </div>
         </div>
             <!--卡片群主框架区-->
         <div class="cardContainer" v-for="(cardGroup,index) in card.cardInfo" :key="cardGroup">
             <!--标题-->
             <h1 :id="'tag' + index">
-                {{ cardGroup.title }}
+                {{ cardGroup[0].section_title }}
             </h1>
             <!--卡片主窗体-->
             <div class="cardGroup">
-                <div class="cardBg" v-for="(card,index) in cardGroup.url"
+                <div class="cardBg" v-for="(card,index) in cardGroup"
                      :key="index"
-                     @click="urlHrefHandler(card.url)">
+                     @click="urlHrefHandler(card.url_link)">
                     <div class="cardBgHref">
                         <div class="cardImages">
                             <el-avatar style="background: #FFFFFF" shape="circle"
-                                       :src="card.avatar_url ? card.avatar_url : squareUrl"/>
+                                       :src="card.url_pic ? card.url_pic : squareUrl"/>
                         </div>
                         <!--卡片文字部分-->
                         <div class="cardTextContent">
-                            <div class="cardTitle"><span>{{ card.title }}</span></div>
-                            <div class="cardSubtitle">{{ card.subTitle }}</div>
+                            <div class="cardTitle"><span>{{ card.url_title }}</span></div>
+                            <div class="cardSubtitle">{{ card.url_remark }}</div>
                         </div>
                     </div>
                 </div>
@@ -46,7 +46,10 @@ import axios from "axios";
 
 const card = reactive({
     isLoading: true,
-    cardInfo: ''
+    // 每个分区内的具体详细信息
+    cardInfo: '',
+    // 每个分区的大标题信息
+    partitionInfo: ''
 })
 
 const squareUrl = ref('https://booop.net/wp-content/uploads/2022/07/booop_logo_250_250_black.png')
@@ -64,15 +67,47 @@ function goAnchor(selector) {
     });
 }
 
+//将card中数组通过section_id进行分类
+const groupBy = (array, f) => {
+    let groups = {};
+    array.forEach(function (o) {
+        var group = JSON.stringify(f(o));
+        groups[group] = groups[group] || [];
+        groups[group].push(o);
+    });
+    return Object.keys(groups).map(function (group) {
+        return groups[group];
+    });
+};
+//将card中数组通过section_id进行分类
+const arrayGroupBy = (list, groupId) => {
+    let sorted = groupBy(list, function (item) {
+        return [item[groupId]];
+    });
+    return sorted;
+};
+
+/**
+ * 根据数组对象的某个字段去重
+ * item.name  是[{name:1}] 根据每条数据的name值来去重
+ * */
+function unique(arr, val) {
+    const res = new Map();
+    return arr.filter(item => !res.has(item[val]) && res.set(item[val], 1))
+}
+
 onMounted(() => {
     axios({
         method: "GET",
-        url: "https://www.fastmock.site/mock/302df2b3fdf407067f03b4121b1b0da9/my/info"
+        url: "http://101.43.207.195:8085/navigation"
     }).then(response => {
+        // console.log(response)
         if (response.status === 200) {
-            // console.log(response)
+            // console.log(response.data)
             card.isLoading = false
-            card.cardInfo = response.data
+            card.cardInfo = arrayGroupBy(response.data.data, 'section_id')
+            // 从card中提取出每个分区的具体大标题
+            card.partitionInfo = unique(response.data.data, 'section_id')
         }
     })
 })
