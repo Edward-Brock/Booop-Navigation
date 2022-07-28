c4337ee025291b10ff969d0153bb6099a3f56c09
Edward-Brock <1416477069@qq.com>
2022-07-28 22:48:41 +0800

    更改页面布局 修改 HomeHeader 显示效果

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,29 @@
 <template>
+  <el-container>
+    <!--顶端栏容器-->
+    <el-header>
+      <HomeHeader/>
+    </el-header>
     <el-container>
-        <el-header height="52px">
-            <HomeHeader/>
-        </el-header>
+      <!--侧边栏容器-->
+      <el-aside class="hidden-xs-only">booop</el-aside>
+      <el-container>
+        <!--主要区域容器-->
         <el-main>
-            <HomeWeather/>
-            <!--<HomeInput/>-->
-            <HomeCard/>
+          <HomeWeather/>
+          <!--<HomeInput/>-->
+          <HomeCard/>
         </el-main>
+        <!--底栏容器-->
         <el-footer>
-            <HomeFooter/>
+          <HomeFooter/>
         </el-footer>
+      </el-container>
     </el-container>
+  </el-container>
 </template>
 
 <script setup>
-import HomeInput from './HomeInput.vue'
 import HomeCard from './HomeCard.vue'
 import HomeHeader from './HomeHeader.vue'
 import HomeWeather from './HomeWeather.vue'
@@ -25,35 +33,44 @@ import HomeFooter from './HomeFooter.vue';
 <style scoped lang="scss">
 // 页面整体宽度
 @mixin width-padding {
-    padding-left: 10%;
-    padding-right: 10%;
-    box-sizing: border-box;
+  padding-left: 5%;
+  padding-right: 5%;
+  box-sizing: border-box;
 }
 
 .el-container {
-    background: #fafafa;
+  background: #fafafa;
+  height: 100vh;
 }
 
 .el-header {
-    //@include width-padding;
-    position: fixed;
-    width: 100%;
-    z-index: 999;
-    -webkit-backdrop-filter: saturate(180%) blur(20px);
-    background: rgba(255, 255, 255, .25);
-    backdrop-filter: saturate(180%) blur(20px);
-    border-bottom: solid 1px var(--el-menu-border-color);
+  @include width-padding;
+  height: 52px;
+  width: calc(100vw);
+  position: fixed;
+  z-index: 999;
+  -webkit-backdrop-filter: saturate(180%) blur(20px);
+  background: rgba(255, 255, 255, .25);
+  backdrop-filter: saturate(180%) blur(20px);
+  border-bottom: solid 1px var(--el-menu-border-color);
+}
+
+.el-aside {
+  height: 100vh;
+  width: 14vw;
+  border-right: solid 1px var(--el-menu-border-color);
 }
 
 .el-main {
-    @include width-padding;
-    margin-top: 60px;
+  @include width-padding;
+  padding-top: 52px;
+  box-sizing: border-box;
 }
 
 .el-footer {
-    @include width-padding;
-    background: #FFFFFF;
-    border-top: solid 1px var(--el-menu-border-color);
+  @include width-padding;
+  background: #FFFFFF;
+  border-top: solid 1px var(--el-menu-border-color);
 }
 
 </style>
--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,20 +1,20 @@
 <template>
-    <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        :ellipsis="false"
-        @select="handleSelect"
-    >
-        <div class="title">booop</div>
-        <div v-show="lightStatus.isShow" class="status_light_group hidden-xs-only">
-            <div class="status_light_display" :class="lightStatus"></div>
-            <div class="status_light_text">{{ projectStatus }}</div>
-        </div>
-        <div class="flex-grow"/>
-        <el-menu-item index="0">首页</el-menu-item>
-        <el-menu-item index="1">博客</el-menu-item>
-    </el-menu>
+  <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+  >
+    <div class="title">booop</div>
+    <div v-show="lightStatus.isShow" class="status_light_group hidden-xs-only">
+      <div class="status_light_display" :class="lightStatus"></div>
+      <div class="status_light_text">{{ projectStatus }}</div>
+    </div>
+    <div class="flex-grow"/>
+    <el-menu-item index="0">首页</el-menu-item>
+    <el-menu-item index="1">博客</el-menu-item>
+  </el-menu>
 </template>
 <script setup>
 import {onMounted, reactive, ref} from 'vue'
@@ -24,128 +24,129 @@ const activeIndex = ref('0')
 
 // 根据标题栏点击数值切换
 function handleSelect(value) {
-    switch (value) {
-        case '0':
-            window.location.reload();
-            break;
-        case '1':
-            window.open("https://www.booop.net", '_self')
-            break;
-    }
+  switch (value) {
+    case '0':
+      window.location.reload();
+      break;
+    case '1':
+      window.open("https://www.booop.net", '_self')
+      break;
+  }
 }
 
 // 数据库连接状态指示
 let projectStatus = ref("")
 const lightStatus = reactive({
-    isShow: true,
-    isNormal: false,
-    isError: false
+  isShow: true,
+  isNormal: false,
+  isError: false
 })
 onMounted(() => {
-    axios({
-        method: "GET",
-        url: "https://api.booop.net/navigation"
-    }).then(response => {
-        console.log(response)
-        if (response.status !== 200 || response.data.status !== 0) {
-            projectStatus.value = response.statusText
-            lightStatus.isError = true
-            lightStatus.isNormal = false
-        } else {
-            projectStatus.value = response.status + " - " + response.data.message
-            lightStatus.isError = false
-            lightStatus.isNormal = true
-        }
-    })
-    setTimeout(function () {
-        lightStatus.isShow = false
-    }, 3000)
+  axios({
+    method: "GET",
+    url: "https://api.booop.net/navigation"
+  }).then(response => {
+    console.log(response)
+    if (response.status !== 200 || response.data.status !== 0) {
+      projectStatus.value = response.statusText
+      lightStatus.isError = true
+      lightStatus.isNormal = false
+    } else {
+      projectStatus.value = response.status + " - " + response.data.message
+      lightStatus.isError = false
+      lightStatus.isNormal = true
+    }
+  })
+  setTimeout(function () {
+    lightStatus.isShow = false
+  }, 3000)
 })
 </script>
 <style scoped lang="scss">
 @mixin fontFamily {
-    font-family: "Century Gothic", "Sitka Text", "PingFang SC", "苹方", "思源黑体", "华文中宋";
+  font-family: "Century Gothic", "Sitka Text", "PingFang SC", "苹方", "思源黑体", "华文中宋";
 }
 
 * {
-    @include fontFamily;
+  @include fontFamily;
 }
 
 .el-menu-demo {
-    height: 100%;
-    padding: 0 9%;
-    @media only screen and (max-width: 992px) {
-        padding: 0 8.5%;
-    }
-    @media only screen and (max-width: 768px) {
-        padding: 0 4.5%;
-    }
+  height: 100%;
 }
 
 
 .el-menu {
-    background-color: rgba(255, 255, 255, .72) !important;
+  background-color: rgba(255, 255, 255, .72) !important;
 }
 
 .el-menu--horizontal {
-    background: none !important;
-    border-bottom: solid 0 var(--el-menu-border-color) !important;
-    align-items: center;
+  background: none !important;
+  border-bottom: solid 0 var(--el-menu-border-color) !important;
+  align-items: center;
+}
+
+.el-menu--horizontal > .el-menu-item.is-active {
+  border-bottom: 0 solid var(--el-menu-active-color) !important;
+}
+
+.el-menu-item:not(.is-disabled):focus, .el-menu--horizontal .el-menu-item:not(.is-disabled):hover {
+  background: none !important;
 }
 
 .title {
-    display: flex;
-    align-self: center;
-    font-size: 1.25em;
-    font-weight: bold;
+  display: flex;
+  align-self: center;
+  font-size: 1.25em;
+  font-weight: bold;
 }
 
 .status_light_group {
-    font-size: 12px;
-    display: flex;
-    align-items: center;
-    flex-wrap: nowrap;
-    background: rgba(220, 220, 220, .25);
-    margin-left: 8px;
-    padding: 4px 4px;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  background: rgba(220, 220, 220, .25);
+  margin-left: 8px;
+  padding: 4px 4px;
+  border-radius: 50px;
+  box-sizing: border-box;
+
+  .status_light_display {
+    width: 10px;
+    height: 10px;
+    background: #888888;
     border-radius: 50px;
-    box-sizing: border-box;
+    margin: 4px;
+  }
 
-    .status_light_display {
-        width: 10px;
-        height: 10px;
-        background: #888888;
-        border-radius: 50px;
-        margin: 4px;
-    }
+  .isNormal {
+    background: lime;
+  }
 
-    .isNormal {
-        background: lime;
-    }
+  .isError {
+    background: red;
+  }
 
-    .isError {
-        background: red;
-    }
-
-    .status_light_text {
-        color: rgba(0, 0, 0, .75);
-        margin-right: 4px;
-    }
+  .status_light_text {
+    color: rgba(0, 0, 0, .75);
+    margin-right: 4px;
+  }
 }
 
 .flex-grow {
-    flex-grow: 1;
+  flex-grow: 1;
 }
 
 .circleAvatar {
-    display: flex;
-    align-items: center;
-    cursor: pointer;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 
-    span {
-        font-size: 14px;
-        margin-left: 8px;
-    }
+  span {
+    font-size: 14px;
+    margin-left: 8px;
+  }
 
 }
 </style>
