316cc281012e368e91959d850d9cc575531d2c80
Edward-Brock <1416477069@qq.com>
2022-07-29 17:02:14 +0800

    HomeFooter样式优化

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container">
-        <div>© 2022 booop</div>
-        <div>Time tries all things.</div>
-    </div>
+  <div class="container">
+    <div>© 2022 booop</div>
+    <div>Time tries all things.</div>
+  </div>
 </template>
 
 <script setup>
@@ -11,17 +11,17 @@
 
 <style scoped lang="scss">
 @mixin fontFamily {
-    color: #AAAAAA;
-    font-size: 14px;
-    font-family: "Bahnschrift", "PingFang SC", "苹方", "思源黑体", "思源宋体 CN";
+  color: #AAAAAA;
+  font-size: 14px;
+  font-family: "Bahnschrift", "PingFang SC", "苹方", "思源黑体", "思源宋体 CN";
 }
 
 .container {
-    @include fontFamily;
-    padding: 20px 0;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  @include fontFamily;
+  padding: 30px 0 20px 0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
